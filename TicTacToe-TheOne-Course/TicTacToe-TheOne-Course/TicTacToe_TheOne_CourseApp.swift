71b3dff3cd05811226843ab5a6b6858e3bc3315b
Raul Gutierrez Niubo <raulgniubo@gmail.com>
2025-09-18 09:33:29 -0400

    Created Xcode Project

---
+++ TicTacToe-TheOne-Course/TicTacToe-TheOne-Course/TicTacToe_TheOne_CourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToe_TheOne_CourseApp.swift
+//  TicTacToe-TheOne-Course
+//
+//  Created by Raul Gutierrez Niubo on 9/18/25.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToe_TheOne_CourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
